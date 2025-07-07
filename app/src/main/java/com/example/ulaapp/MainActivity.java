be472fc660137bb5f4f6cd0455c743f21afbc22e
admin <nyam6771@gmail.com>
2025-07-07 19:03:50 +0300

    lil changes

--- app/src/main/java/com/example/ulaapp/MainActivity.java
+++ app/src/main/java/com/example/ulaapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ulaapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // variable initialization
+    LinearLayout toMoviePageBtn, toConcertPageBtn;
+    ImageView toInfoPage, toSettingsPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +28,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // set layout elements to variables
+
+        toMoviePageBtn = findViewById(R.id.MovieOptionContainer);
+        toConcertPageBtn = findViewById(R.id.ConcertOptionContainer);
+        toInfoPage = findViewById(R.id.InfoButton);
+        toSettingsPage = findViewById(R.id.SettingsButton);
+
+        // click listeners
+
+        toMoviePageBtn.setOnClickListener(V-> {
+            Log.d("ClickEvent", "Click on MOVIE option");
+        });
+        toConcertPageBtn.setOnClickListener(V-> {
+            Log.d("ClickEvent", "Click on CONCERT option");
+        });
+        toInfoPage.setOnClickListener(V-> {
+            Log.d("ClickEvent", "info btn");
+        });
+        toSettingsPage.setOnClickListener(V-> {
+            Log.d("ClickEvent", "settings btn");
+        });
+
+
     }
-
-
-
 }
